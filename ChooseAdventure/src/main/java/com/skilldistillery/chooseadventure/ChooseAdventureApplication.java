4e7a37cc2f1e5d300a59dd84393f5261c8c7c255
jdebroux <jtdebroux@gmail.com>
2019-09-12 14:20:15 -0600

    create spring boot project

---
+++ ChooseAdventure/src/main/java/com/skilldistillery/chooseadventure/ChooseAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.chooseadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseAdventureApplication.class, args);
+	}
+
+}
