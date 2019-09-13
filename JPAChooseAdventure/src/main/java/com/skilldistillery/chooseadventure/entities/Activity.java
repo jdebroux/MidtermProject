e22e55eed8e9f25f94463a2cffb0737adeaaab86
jdebroux <jtdebroux@gmail.com>
2019-09-13 13:58:34 -0600

    added half the entities and functioning controller

---
+++ JPAChooseAdventure/src/main/java/com/skilldistillery/chooseadventure/entities/Activity.java
@@ -0,0 +1,74 @@
+package com.skilldistillery.chooseadventure.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Activity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	@Column(name = "link_wiki")
+	private String link;
+
+	public Activity() {
+	}
+
+	public Activity(String name, String link) {
+		super();
+		this.name = name;
+		this.link = link;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Activity other = (Activity) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", name=" + name + ", link=" + link + "]";
+	}
+}
---
+++ JPAChooseAdventure/src/main/java/com/skilldistillery/chooseadventure/entities/NationalPark.java
@@ -0,0 +1,95 @@
+package com.skilldistillery.chooseadventure.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class NationalPark {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	@Column(name = "location_id")
+	private int locationId;
+	@Column(name = "link_nps")
+	private String link;
+
+	public NationalPark() {
+	}
+
+	public NationalPark(String name, String description, int locationId, String link) {
+		this.name = name;
+		this.description = description;
+		this.locationId = locationId;
+		this.link = link;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(int locationId) {
+		this.locationId = locationId;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NationalPark other = (NationalPark) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "NationalPark [id=" + id + ", name=" + name + ", description=" + description + ", locationId="
+				+ locationId + ", link=" + link + "]";
+	}
+}
